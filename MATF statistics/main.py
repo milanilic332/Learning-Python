c9e6be8ec3ab13ceaed4bb4bf8f69de738f51b3c
Milan Ilic <milanilic332@gmail.com>
2018-03-09 12:57:43 +0100

    Dodata imena i prezimena sa alasa

--- MATF statistics/main.py
+++ MATF statistics/main.py
@@ -1,5 +1,5 @@
 import urllib.request as urlr
-import sys, re
+import sys, re, os
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -11,6 +11,42 @@ def main():
 
 		firstLetterDist(names)
 		letterDist(names)
+		fullnamesAlas()
+
+		plt.show()
+		
+def fullnamesAlas():
+	with open('list.txt', 'r') as f:
+		hypatia = f.read()
+		Names = {}
+		n = re.compile(r'[^\']+\'[^\']+\'[^\']+\'([^\']+)\'\]')
+		for match in n.finditer(hypatia):
+			lst = match.group(1).split(' ')
+			for i in lst:
+				if i in Names:
+					Names[i] = Names[i] + 1
+				else:
+					Names[i] = 1
+
+		sortedNamesLst = sorted(Names.items(), key=lambda x: x[1])[::-1]
+		sortedNamesLst = sortedNamesLst[:40]
+		sortedNamesLst = sortedNamesLst[::-1]
+		sortedNamesMap = {}
+
+		for ind, (i, j) in enumerate(sortedNamesLst):
+			if ind != 39:
+				sortedNamesMap[i] = j
+			else:
+				break
+
+		pos = np.arange(len(sortedNamesMap.values())) + 0.5
+
+		plt.figure(3)
+		plt.barh(pos, list(sortedNamesMap.values()), align = 'center')
+		plt.yticks(pos, sortedNamesMap.keys())
+		plt.ylabel('Imena')
+		plt.xlabel('Broj pojavljivanja')
+		plt.title('Raspodela imena na alasu')
 
 def findNames(emp, html):
 	names = []
@@ -28,17 +64,19 @@ def firstLetterDist(names):
 	
 	for name in names:
 		firstLetters[name[0]] = firstLetters.get(name[0], 0) + 1
-		sortedFirstLettersList = sorted(firstLetters.items(), key=lambda x: x[1])[::-1]
+
+	sortedFirstLettersList = sorted(firstLetters.items(), key=lambda x: x[1])[::-1]
+	
 	sortedFirstLetters = {}
 	
 	for (a, b) in sortedFirstLettersList:
 		sortedFirstLetters[a] = b
-	
+
+	plt.figure(2)
 	plt.bar(sortedFirstLetters.keys(), sortedFirstLetters.values())
 	plt.ylabel('Broj pojavljivanja')
 	plt.xlabel('Slova')
 	plt.title('Raspodela pocetnih slova u imenu')
-	plt.show()
 
 def letterDist(names):
 	letters = {}
@@ -49,17 +87,18 @@ def letterDist(names):
 			if let not in '.- ':
 				let = let.upper()
 				letters[let] = letters.get(let, 0) + 1
-		sortedLettersList = sorted(letters.items(), key=lambda x: x[1])[::-1]
+	
+	sortedLettersList = sorted(letters.items(), key=lambda x: x[1])[::-1]
 	sortedLetters = {}
 	
 	for (a, b) in sortedLettersList:
 		sortedLetters[a] = b
-	
+
+	plt.figure(1)
 	plt.bar(sortedLetters.keys(), sortedLetters.values())
 	plt.ylabel('Broj pojavljivanja')
 	plt.xlabel('Slova')
 	plt.title('Raspodela svih slova u imenu i prezimenu')
-	plt.show()	
 
 if __name__ == '__main__':
 	main()
